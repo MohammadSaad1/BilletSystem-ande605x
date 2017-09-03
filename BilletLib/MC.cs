205bef8cfda4d4f30ca94ca79a72aa948d42cd93
Anders Thomsen <ande605x@edu.easj.dk>
2017-09-04 01:55:26 +0200

    Opgave 2

---
+++ BilletLib/MC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BilletLib
+{
+    public class MC
+    {
+        public string Nummerplade { get; set; }
+        public DateTime Date { get; set; }
+
+        public int Pris()
+        {
+            return 120;
+        }
+
+        public string Køretøj()
+        {
+            return "MC";
+        }
+    }
+}

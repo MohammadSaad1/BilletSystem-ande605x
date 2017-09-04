a4081da1221ef9075af2d559e94eadb6ff89f396
Anders Thomsen <ande605x@edu.easj.dk>
2017-09-04 02:13:30 +0200

    Opgave 2 inkl. tests

---
+++ BilletLibTest/MCTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BilletLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BilletLibTest
+{
+    [TestClass]
+    public class MCTests
+    {
+        [TestMethod]
+        public void TestPris120()
+        {
+            //Arrange
+            var mc = new MC();
+            //Act
+            int pris = mc.Pris();
+            //Assert
+            Assert.AreEqual(120, pris);
+        }
+
+
+        [TestMethod]
+        public void TestKøretøjMC()
+        {
+            //Arrange
+            var mc = new MC();
+            //Act
+            string køretøj = mc.Køretøj();
+            //Assert
+            Assert.AreEqual("MC", køretøj);
+        }
+
+    }
+}
